773d4d4940da2924ef43d8e4b731aaf4469c914b
32comma <louis.develog@gmail.com>
2022-06-18 19:06:26 +0900

    update

--- app.js
+++ app.js
@@ -3,9 +3,28 @@ let painting = false;
 canvas.width = 700;
 canvas.height = 700;
 const ctx = canvas.getContext("2d");
-
+var colors = document.getElementsByClassName("jsColor");
+//console.log(Array.from(colors));
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
+const range = document.getElementById("jsRange");
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
+if (range) {
+    range.addEventListener('input', handleRangeChange);
+}
+
+function handleRangeChange(e) {
+    console.log(e.target.value);
+    ctx.lineWidth = e.target.value;
+}
+
+function handleColorClick(e) {
+    console.log(e);
+    const color = e.target.style.backgroundColor;
+    console.log(color);
+    ctx.strokeStyle = color;
+}
 
 function stopPainting() {
     painting = false;
